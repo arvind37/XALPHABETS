c64ad8f7738d0cd4bd7bd91eb7c8607f6dfe5b82
Arvind <arvind.kumawat37@gmail.com>
2026-01-01 02:01:09 +0530

    added alphabet project

---
+++ script.js
@@ -0,0 +1,17 @@
+const inputEle = document.querySelector('.input');
+const backbtnEle = document.querySelector('.backbtn');
+const beforeSearchEle = document.querySelector('.before-search');
+const keyEle = document.querySelectorAll('.key');
+
+
+keyEle.forEach((key) => {
+  key.addEventListener('click', () => {
+    inputEle.value += key.innerHTML;
+  })
+});
+
+backbtnEle.addEventListener('click', () => {
+  if(inputEle.value.length > 0) {
+    inputEle.value = inputEle.value.slice(0, inputEle.value.length - 1);
+  }
+});
